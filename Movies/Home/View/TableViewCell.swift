89d5cad688d7ce5b406ff79b17c8932ab22e331c
Pamela Ianovalli <pamela.novalli@gmail.com>
2019-06-19 15:57:07 -0300

    First commit

---
+++ Movies/Home/View/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Movies
+//
+//  Created by Pamela Ianovalli on 19/06/19.
+//  Copyright © 2019 Pamela Ianovalli. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Movies/MovieDetail/Controller/MovieViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MovieViewController.swift
+//  Movies
+//
+//  Created by Pamela Ianovalli on 18/06/19.
+//  Copyright © 2019 Pamela Ianovalli. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var lblDuration: UILabel!
+    @IBOutlet weak var lblCategory: UILabel!
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var lblRating: UILabel!
+    @IBOutlet weak var tvSummary: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
